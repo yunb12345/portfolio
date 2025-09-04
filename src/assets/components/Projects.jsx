d1ffd4c9d78dccc82709452321b5abb9edf5d40b
yunb12345 <agusyoon11@hotmail.com>
2025-09-04 19:27:42 -0300

    cambio en el titulo de la pestaña y el icono. Ahora los proyectos tienen img

--- src/assets/components/Projects.jsx
+++ src/assets/components/Projects.jsx
@@ -1,20 +1,20 @@
 const projects = [
   {
-    img: "",
+    img: "/img/Splitify.png",
     name: "Splitify",
     description:
       "Frontend y Backend de una aplicación web para dividir gastos en grupos de amigos...",
     tags: ["React","SQL","Node","Express","Sequelize","TailwindCSS"],
   },
   {
-    img: "",
+    img: "/img/facturalo.png",
     name: "Facturalo",
     description:
       "Un chatbot impulsado por IA que ayuda a los usuarios a generar facturas electrónicas de manera rápida y sencilla con lenguaje natural...",
     tags: ["Python","Whatsapp API","OpenAI","Flask"],
   },
   {
-    img: "",
+    img: "/img/CookingBook.png",
     name: "CookingBook",
     description:
       "Una aplicacion movil para crear recetas, buscar recetas y clasificarlas",
@@ -30,12 +30,9 @@ export default function Projects() {
       <ol className="group/list">
         {projects.map((exp, idx) => (
           <li key={idx} className="relative mb-12">
-            <div className="group relative grid pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
+            <div className="group relative grid gap-4 pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
               <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
-              <header className="z-10 mb-2 mt-1 text-xs font-semibold uppercase tracking-wide text-slate-500 sm:col-span-2">
-                {exp.date}
-              </header>
-              <div className="z-10 sm:col-span-6">
+              <div className="z-10 sm:order-2 sm:col-span-6">
                 <h3 className="text-lg font-semibold text-white">
                   <a className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300  group/link text-base">
                     <span className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block">
@@ -55,6 +52,7 @@ export default function Projects() {
                   ))}
                 </ul>
               </div>
+              <img src={exp.img} alt={exp.name} width={200} height={48} className="aspect-video object-cover rounded border-2 border-slate-200/10 transition group-hover:border-slate-200/30 sm:order-1 sm:col-span-2 sm:translate-y-1" />
             </div>
           </li>
         ))}
